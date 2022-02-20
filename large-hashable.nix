88576ae7006dbf65094dc9dd17e9a1548aaf17e9
sternenseemann <sternenseemann@systemli.org>
2022-02-20 23:42:43 +0100

    Regenerate large-hashable.nix
    This allows the Nix expression to work with recent nixpkgs again.

--- large-hashable.nix
+++ large-hashable.nix
@@ -1,22 +1,29 @@
-{ mkDerivation, aeson, base, base16-bytestring, bytes, bytestring
-, containers, hashable, HTF, QuickCheck, scientific, stdenv, strict
-, template-haskell, text, time, transformers, unordered-containers
-, utf8-light, vector, void
+{ mkDerivation, aeson, base, base16-bytestring, byteable, bytes
+, bytestring, cereal, containers, criterion, cryptohash, cryptonite
+, deepseq, hashable, HTF, inspection-testing, lib, memory
+, QuickCheck, safecopy, scientific, strict, template-haskell, text
+, time, transformers, unordered-containers, utf8-light, vector
+, void
 }:
 mkDerivation {
   pname = "large-hashable";
-  version = "0.1.0.3";
+  version = "0.1.0.4";
   src = ./.;
   libraryHaskellDepends = [
-    aeson base base16-bytestring bytes bytestring containers scientific
-    strict template-haskell text time transformers unordered-containers
-    utf8-light vector void
+    aeson base base16-bytestring bytes bytestring containers cryptonite
+    memory scientific strict template-haskell text time transformers
+    unordered-containers utf8-light vector void
   ];
   testHaskellDepends = [
-    aeson base bytes bytestring containers hashable HTF QuickCheck
-    scientific strict text time unordered-containers vector
+    aeson base bytes bytestring containers hashable HTF
+    inspection-testing QuickCheck scientific strict text time
+    unordered-containers vector
+  ];
+  benchmarkHaskellDepends = [
+    base base16-bytestring byteable bytes bytestring cereal criterion
+    cryptohash deepseq safecopy text transformers
   ];
   homepage = "https://github.com/factisresearch/large-hashable";
   description = "Efficiently hash (large) Haskell values";
-  license = stdenv.lib.licenses.bsd3;
+  license = lib.licenses.bsd3;
 }
